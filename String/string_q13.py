ca3c8df3dae2b568e0b4130cc367cbad4e473047
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-23 20:31:54 +0530

    string to uppercase n lowercase

---
+++ String/string_q13.py
@@ -0,0 +1,8 @@
+# string to uppercase n lowercase
+s=input("Enter a string: ")
+u=s.upper()
+l=s.lower()
+print("Uppercase:", u)
+print("Lowercase:", l)
+if u==s and l==s:
+    print("String contains only uppercase or lowercase.")
