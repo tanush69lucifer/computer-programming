f31dc987a03eb610a80ee610133d994b14ae73d8
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-23 19:12:56 +0530

    Update function_q19.py

--- Functions/function_q19.py
+++ Functions/function_q19.py
@@ -1,5 +1,5 @@
-# checking if str has digit
-s=input("Enter a string: ")
-def is_digit(s):
-    return s.isdigit()
-print(f"Contains only digits? {is_digit(s)}")
+# list to string
+lst=input("Enter elements separated by commas: ").split(",")
+def list_to_string(lst):
+    return ''.join(lst)
+print("Converted string:", list_to_string(lst))
