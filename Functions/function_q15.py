ae9b6b3e5079599a107a21719b7693657a1d3f4f
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-23 20:46:03 +0530

    smallest element in a list

--- Functions/function_q15.py
+++ Functions/function_q15.py
@@ -1,4 +1,4 @@
-# smallest element in a list
+# smallest element in a list . 
 lst=list(map(int, input("Enter a list of numbers separated by commas: ").split(",")))
 def smallest_element(lst):
     return min(lst)
