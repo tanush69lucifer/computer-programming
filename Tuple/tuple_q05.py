ae86f0eb76c533acc5187eaadb1662586f4474df
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-23 19:38:35 +0530

    tuple_q05.py

---
+++ Tuple/tuple_q05.py
@@ -0,0 +1,4 @@
+# concateinating two tuples
+t1=tuple(input("Enter first tuple (comma separated): ").split(","))
+t2=tuple(input("Enter second tuple (comma separated): ").split(","))
+print("Concatenated tuple:", t1+t2)
