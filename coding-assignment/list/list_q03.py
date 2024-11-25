d36b8df322d01e93c04901e82c09399daa4da9c2
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:22:59 +0530

    occurances of element of a list

--- coding-assignment/list/list_q03.py
+++ coding-assignment/list/list_q03.py
@@ -1,4 +1,5 @@
 # occurances of element of a list
+# lst
 lst=input("Enter elements separated by commas: ").split(",")
 e=input("Enter the element to count: ")
 print(f"Occurrences of '{e}':", lst.count(e))
