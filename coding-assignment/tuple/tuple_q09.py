8b354732a966c1fd99084f9adacc19adaa313cf0
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:15:30 +0530

    removing duplicate elements from tuple

--- coding-assignment/tuple/tuple_q09.py
+++ coding-assignment/tuple/tuple_q09.py
@@ -1,4 +1,5 @@
 # removing duplicate elements from tuple
+# tuple
 t=tuple(input("Enter a list of items separated by commas: ").split(","))
 t_no_duplicates=tuple(sorted(set(t), key=t.index))
 print("Tuple without duplicates:", t_no_duplicates)
