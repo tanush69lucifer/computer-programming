9f6a461d720a26917cb9dbbe7a4c891a54ce27dc
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:27:04 +0530

    second largest element ina  list

--- coding-assignment/list/list_q17.py
+++ coding-assignment/list/list_q17.py
@@ -1,4 +1,5 @@
 # second largest element ina  list
+# lst 
 lst=list(map(int, input("Enter a list of numbers separated by commas: ").split(",")))
 lst.sort()
 print("Second largest element:", lst[-2])
