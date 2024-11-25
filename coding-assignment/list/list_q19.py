de3bf8cf8d82081936ef254d856242fbca28d35a
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:27:36 +0530

    checking if lisst is a sublist of another list

--- coding-assignment/list/list_q19.py
+++ coding-assignment/list/list_q19.py
@@ -1,5 +1,5 @@
 # checking if lisst is a sublist of another list
-
+# lst
 lst1=input("Enter the first list (comma separated): ").split(",")
 lst2=input("Enter the second list (comma separated): ").split(",")
 if all(i in lst1 for i in lst2):
