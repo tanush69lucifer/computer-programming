5c607075741af404cf8d5f43f7c83fe00307874d
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:35:30 +0530

    getting value of a specific key

--- coding-assignment/dict/dict_q02.py
+++ coding-assignment/dict/dict_q02.py
@@ -1,4 +1,5 @@
 # getting value of a specific key
+# dct 
 d = dict(input("Enter key-value pairs (key:value) separated by commas: ").split(","))
 k = input("Enter the key to fetch its value: ")
 if k in d:
