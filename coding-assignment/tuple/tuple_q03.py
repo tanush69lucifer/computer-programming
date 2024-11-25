f5fe76d070c1693347b8bfdddc217bb39f68c978
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:13:35 +0530

    occurances of element in tuple

--- coding-assignment/tuple/tuple_q03.py
+++ coding-assignment/tuple/tuple_q03.py
@@ -1,4 +1,5 @@
 # occurances of element in tuple
+# tuple
 t=tuple(input("Enter a list of items separated by commas: ").split(","))
 e=input("Enter the element to count: ")
 print(f"Occurrences of '{e}':", t.count(e))
