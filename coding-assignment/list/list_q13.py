ad3bbb03dc131f7b50b6381a16fa9d6802482d15
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2024-11-25 17:25:41 +0530

    removing duplicates from a list

--- coding-assignment/list/list_q13.py
+++ coding-assignment/list/list_q13.py
@@ -1,4 +1,5 @@
 # removing duplicates from a list
+# lst 
 lst=input("Enter elements separated by commas: ").split(",")
 lst_no_duplicates=list(set(lst))
 print("List without duplicates:", lst_no_duplicates)
